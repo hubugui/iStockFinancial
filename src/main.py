b04228f568bb554215105f5b9766a71f5c7c3619
hubugui <hubugui@gmail.com>
2012-07-27 17:19:40 +0800

    rm pygments

--- src/main.py
+++ src/main.py
@@ -13,7 +13,4 @@ def main(argv):
 		print("for example: 'python main.py 2011 /opt/istock'")
 
 if __name__ == '__main__':
-	import stacktracer
-	stacktracer.trace_start("trace.html",interval=5,auto=True)
-
 	main(sys.argv)
