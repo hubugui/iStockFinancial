4f87d90a7d8b1991d26513c87345d3fe4d4ec59a
wangyang <wangyang@apollo.(none)>
2012-08-07 11:57:44 +0800

    client install

---
+++ client/install.sh
@@ -0,0 +1,7 @@
+wget -O urllib3-V1.4.zip https://github.com/shazow/urllib3/zipball/1.4
+unzip urllib3-V1.4.zip
+cd shazow-urllib3-1ffd99e
+sudo python setup.py install
+cd ..
+sudo rm -rf shazow-urllib3-1ffd99e
+sudo rm -rf urllib3-V1.4.zip
